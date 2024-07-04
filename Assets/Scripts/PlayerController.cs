139cb0897dc65f019e7b840a5d5eec6619e3c4a6
GAJAM JYOTHSNA <96329766+Gajamjyothsna@users.noreply.github.com>
2024-07-04 11:34:57 +0530

    Cinemachine is added to the player & added basic quick setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,6 +23,15 @@ namespace TPSGame
             Vector3 movement = new Vector3(horizontalValue, 0, verticalValue) * _playerSpeed * Time.deltaTime;
 
             transform.Translate(movement);
+
+            if(movement.magnitude > 0 )
+            {
+                _playerAnimator.SetFloat("moveAmount", 1);
+            }
+            else if(movement.magnitude <= 0 )
+            {
+                _playerAnimator.SetFloat("moveAmount", 0);
+            }
         }
     }
 }
